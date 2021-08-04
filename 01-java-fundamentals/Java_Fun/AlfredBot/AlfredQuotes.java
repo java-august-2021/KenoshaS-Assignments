3e715d4c70a9e8fc9c0540721f4eea995b786682
Kenosha Guthrie <kenoshaguthrie@gmail.com>
2021-08-03 21:05:27 -0500

    Added Java Fun

---
+++ 01-java-fundamentals/Java_Fun/AlfredBot/AlfredQuotes.java
@@ -0,0 +1,7 @@
+
+public class AlfredQuotes{
+    public String basicGreeting(){
+    
+    }
+
+}
---
+++ 01-java-fundamentals/Java_Fun/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgram{
+    public static void main(String[] args) {
+        System.out.println("My name is Kenosha Guthrie");
+        System.out.println("I am 30 years old");
+        System.out.println("My hometown is Joliet, IL");
+    }
+
+}
