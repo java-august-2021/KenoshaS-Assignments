f69a02cd380298b707401151ca599476f979547a
Kenosha Guthrie <kenoshaguthrie@gmail.com>
2021-08-08 21:04:37 -0500

    Added Basics

---
+++ 01-java-fundamentals/05-basics/Basics.java
@@ -0,0 +1,44 @@
+public class Basics{
+    public static void main(String[] args){
+        OneTo255();
+        OddTo255();
+    }
+    public static void OneTo255(){
+        for(int i = 1; i <= 255; i++){
+            System.out.println(i);
+        }
+    }
+    public static void OddTo255(){
+        for(int i = 1; i <= 255; i++){
+            if(i % 2 !=0 ){
+                System.out.println(i);
+            }
+        }
+    }
+    public static void SumTo255(int[] num){
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum += num[i];
+        }
+
+    }
+    public static void IterateArray(){
+        var sum = 0;
+    }
+}
+    public static void FindMax(int[] num){
+        int max = Integer.MIN_VALUE;
+        for(int val: arr){
+            if (val > Max)
+            max = val;
+    }
+    public static ArrayList<Integer> OddArray(){
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        for (int i = 0; i <= 255; i++){
+            if(i % 2 != 0)
+                nums.add(i)
+        }
+    }
+
+
+
